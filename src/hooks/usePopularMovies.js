3a35112646368df4415de6d8b92f04c25a3a4c4e
priyanka <tiwary.priyanka92Z@gmail.com>
2024-01-11 23:51:58 +0100

    performance optimization

--- src/hooks/usePopularMovies.js
+++ src/hooks/usePopularMovies.js
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { API_OPTIONS } from "../utils/constants";
 
 import { addPopularMovies } from "../slices/movieSlice";
 
 const usePopularMovies = () => {
     const dispatch = useDispatch();
+    const popularMovies = useSelector((store)=> store.movies.popularMovies)
+
     const getPopularMovies =  async()=>{
         const data  = await fetch("https://api.themoviedb.org/3/movie/popular?page=1", API_OPTIONS);
         const json = await data.json();
@@ -13,7 +15,7 @@ const usePopularMovies = () => {
       }
     
       useEffect(()=>{
-        getPopularMovies();
+        popularMovies && getPopularMovies();
       },[]);
 
 }
--- src/hooks/useUpcomingMovies.js
+++ src/hooks/useUpcomingMovies.js
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { API_OPTIONS } from "../utils/constants";
 
 import { addUpcomingMovies } from "../slices/movieSlice";
 
 const useUpcomingMovies = () => {
     const dispatch = useDispatch();
+    const upcomingMovies = useSelector((store)=> store.movies.upComingMovies)
+
     const getUpcomingMovies =  async()=>{
         const data  = await fetch("https://api.themoviedb.org/3/movie/upcoming?page=1", API_OPTIONS);
         const json = await data.json();
@@ -13,7 +15,7 @@ const useUpcomingMovies = () => {
       }
     
       useEffect(()=>{
-        getUpcomingMovies();
+        upcomingMovies && getUpcomingMovies();
       },[]);
 
 }
